eac051e6416b495f769464ac2938464e5dbce28c
VinoliaMoekwa <moe23001@byui.edu>
2023-09-23 02:31:53 +0200

    Homepage 1 edits

--- homepage/scripts/getdates.js
+++ homepage/scripts/getdates.js
@@ -1,2 +1,13 @@
-alert(document.lastModified);
+
 // returns: Tuesday, December 16, 2017 11:09:42
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the current year
+    const currentYear = new Date().getFullYear();
+    
+    // Get the last modified date and time
+    const lastModified = new Date(document.lastModified);
+    
+    // Update the HTML elements with the obtained values
+    document.getElementById("currentYear").textContent = currentYear;
+    document.getElementById("lastModified").textContent = lastModified.toLocaleString();
+});
