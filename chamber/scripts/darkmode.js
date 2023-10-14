a192f4ac266068c625083cc817bed0fe29252c5e
VinoliaMoekwa <moe23001@byui.edu>
2023-10-14 17:40:48 +0200

    Chamber page enhancement

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,1 +1,12 @@
+var darkmode = document.getElementById("icon");
+
+darkmode.onclick = function(){
+
+    document.body.classList.toggle("darktheme");
+    if (document.body.classList.contains ("darktheme")){
+        icon.src = "images/lightmode.png";
+    }else {
+    icon.src="images/darkmode.png";
+    }
+}
 
