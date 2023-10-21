82e707a1e9c4269052c2d43c92ebc753de128a20
VinoliaMoekwa <moe23001@byui.edu>
2023-10-21 22:55:14 +0200

    Week06

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,21 @@
+// windchill.js
+
+function calculateWindChill(temperatureCelsius, windSpeed) {
+    const temperatureFahrenheit = (temperatureCelsius * 9/5) + 32;
+  
+    if (temperatureCelsius <= 10) {
+      if (windSpeed > 4.8) { 
+        const windChillCelsius = 13.12 + 0.6215 * temperatureCelsius - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperatureCelsius * Math.pow(windSpeed, 0.16);
+        const windChillFahrenheit = (windChillCelsius * 9/5) + 32;
+        return windChillCelsius.toFixed(2) + "°C (" + windChillFahrenheit.toFixed(2) + "°F)";
+      } else {
+        return "N/A";
+      }
+    } else {
+
+      return "N/A";
+    }
+  }
+  
+        
+    
