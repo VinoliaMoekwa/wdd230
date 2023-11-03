256bc99d186c512d075e30e7b93193e28485c4c8
VinoliaMoekwa <moe23001@byui.edu>
2023-11-03 23:16:55 +0200

    week6 updates

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,36 +1,23 @@
 // windchill.js
 
-function calculateWindChill(temperatureCelsius, windSpeed) {
-    const temperatureFahrenheit = (temperatureCelsius * 9/5) + 32;
-  
-    if (temperatureCelsius <= 10) {
-      if (windSpeed > 4.8) { 
-        const windChillCelsius = 13.12 + 0.6215 * temperatureCelsius - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperatureCelsius * Math.pow(windSpeed, 0.16);
-        const windChillFahrenheit = (windChillCelsius * 9/5) + 32;
-        return windChillCelsius.toFixed(2) + "°C (" + windChillFahrenheit.toFixed(2) + "°F)";
-      } else {
-        return "N/A";
-      }
-    } else {
-
-      return "N/A";
-    }
-      const temperatureInput = document.getElementById("temperature");
-      const windSpeedInput = document.getElementById("wind-speed");
-      const windChillValue = document.getElementById("wind-chill-value");
-
-      temperatureInput.addEventListener("input", updateWindChill);
-      windSpeedInput.addEventListener("input", updateWindChill);
-
-      function updateWindChill() {
-        const temperature = parseFloat(temperatureInput.value);
-        const windSpeed = parseFloat(windSpeedInput.value);
-
-        const windChill = calculateWindChill(temperature, windSpeed);
-        windChillValue.textContent = windChill;
-      }
+function calculateWindChill(temperature, windSpeed) {
     
+  if (temperature <= 50 && windSpeed > 3.0) {
+    
+    const windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
+    
+    return windChill.toFixed(2); 
+  } else {
+    
+    return "N/A";
   }
-  
-        
-    
+}
+
+
+const currentTemperature = parseFloat(document.getElementById("temperature").textContent);
+const currentWindSpeed = parseFloat(document.getElementById("windSpeed").textContent);
+
+const windChill = calculateWindChill(currentTemperature, currentWindSpeed);
+
+
+document.getElementById("windChillValue").textContent = windChill;
